9971b04262dd3576fa3390d836fa36013f3e3035
Emanuele <Emanuele@DESKTOP-KMUD47R>
2024-05-31 23:36:29 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercitazione_ReadWrite_with_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+        }
+    }
+}
